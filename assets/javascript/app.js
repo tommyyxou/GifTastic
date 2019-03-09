b81a990995d3f5bbf8167755f450f3ca5b79a734
tommyyxou <tommy.yx.ou@gmail.com>
2019-03-09 01:09:11 -0800

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,8 @@ let words = [
     "Fred Flintstone", "The Grinch", "Popeye", "Wile E. Coyote", "Rocky and Bullwinkle",
 ];
 
+let favorites = [];
+
 let searchTerm = "";
 let charId = 0;
 let limit = 10;
@@ -10,9 +12,11 @@ let ratingQuery = "G"
 let webResponse = "";
 let isImgStill = true;
 let topicJSON = "";
+let responsePos = null;
 
 function initialize () {
-    if (localStorage.length > 0) {
+    console.log (localStorage)
+    if (localStorage.length > 1) {
         words = JSON.parse(localStorage.getItem("topic"));
         for (charId = 0; charId < words.length; charId++ ) {
             setupButton (words);
@@ -25,9 +29,10 @@ function initialize () {
     }
 
     displayGif ();
+    displayFav ();
     addButton ();
     clearLocalStorage ();
-    ratingSetting ()
+    ratingSetting ();
     
 };
 
@@ -59,19 +64,8 @@ function addButton () {
     });
 }    
 
-function displayGif () {
-    $(".button").on("click", function() {
-        $("#gifsDiv").remove();
-        $(".leftSideBar").append("<div id='gifsDiv'></div>");
-
-        if (searchTerm == $(this).text()) {
-            limit = limit + 10;
-        } else {
-            limit = 10;
-        }
-
-        searchTerm = $(this).text();
-        let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=2RY8YHlNYX8yKSGzWLcLUwu7fPvEjdp5&q=" +
+function getResults () {
+    let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=2RY8YHlNYX8yKSGzWLcLUwu7fPvEjdp5&q=" +
             searchTerm + "&limit=" +
             limit + "&offset=0&rating=" +
             ratingQuery + "&lang=en";
@@ -81,11 +75,11 @@ function displayGif () {
             method: "GET"
         })
         .then(function(response) {
-            for (let i = 0; i < response.data.length; i++) {
+            for (let i = (limit-10); i < response.data.length; i++) {
                 webResponse = response
-                console.log (response)
                 let rating = response.data[i].rating;
                 let stillImg = response.data[i].images.fixed_height_still.url;
+                let animeImg = response.data[i].images.fixed_height.url;
 
                 let gifIdString = "gif" + i;
                 $("#gifsDiv").append("<div id='" + gifIdString + "' class='gifImage'></div>");
@@ -96,17 +90,97 @@ function displayGif () {
                 let imgIdString = "img" + i;
 
                 let gifImg = $("<img>");
-                gifImg.attr('id', imgIdString )
+                gifImg.attr('id', imgIdString );
+                gifImg.attr('class', "gifImgFrame");
                 gifImg.attr('src', stillImg);
 
                 let appendgifId = "#" + gifIdString;
                 $(appendgifId).append(gifRating);
                 $(appendgifId).append(gifImg);
+                $(appendgifId).append("<div><a href='" + animeImg + "' download='gif'><button class='buttonDL' src='" + animeImg + "'>Download Gif</button></a></div>");
+                $(appendgifId).append("<button id='" + i + "' class='buttonDL float fav' >Add to Favorites</button>");
             }
             gifAnime ();
+            addFav ();
         });
+}
+
+function addFav () {
+    $(".fav").on("click", function() {
+    console.log ("fav click")
+    let i = event.currentTarget.id;
+    //console.log (webResponse.data[i]);
+    
+    let favGif = {  rating: webResponse.data[i].rating,
+                    stillImgURL: webResponse.data[i].images.fixed_height_still.url,
+                    animeImgURL: webResponse.data[i].images.fixed_height.url,
+                 }
+    favorites.push(favGif);
+    //console.log (favorites);
+    favJSON = JSON.stringify(favorites);
+    localStorage.setItem ("favorites", favJSON);
+    displayFav();
     });
     
+}
+
+function displayFav() {
+    $(".favImage").remove();
+    favorites = JSON.parse(localStorage.getItem("favorites"));
+    console.log (favorites)
+    console.log ("fav display")
+    if (favorites.length !== 0) {
+        for (let i = 0; i < favorites.length ; i++) {
+            let favString = "fav" + i
+            
+                $("#favDiv").append("<div id='" + favString + "' class='favImage'></div>");
+      
+                let gifRating = $("<h2>");
+                gifRating.text("Rating: " + favorites[i].rating);
+
+                let imgIdString = "img" + i;
+
+                let gifImg = $("<img>");
+                gifImg.attr('id', imgIdString );
+                gifImg.attr('class', "gifImgFrame");
+                gifImg.attr('src', favorites[i].stillImgURL);
+
+                let appendfavId = "#" + favString;
+                $(appendfavId).append(gifRating);
+                $(appendfavId).append(gifImg);
+        }
+    }
+}
+
+function favReset () {
+    favorites = [];
+    favJSON = JSON.stringify(favorites);
+    localStorage.setItem ("favorites", favJSON);
+    $(".favImage").remove();
+
+
+}
+
+function displayGif () {
+    $(".button").on("click", function() {
+         if (searchTerm !== $(this).text()) {
+        $("#gifsDiv").remove();
+        $("#placeHolder").append("<div id='gifsDiv'></div>");
+        $("#more").remove();
+        searchTerm = $(this).text();
+        getResults();
+        $("#moreButtonDiv").append("<button id='more' class='button first' onclick='moreGif();'>Load More Gifs</button>")
+        }
+        
+    });   
+}
+
+function moreGif () {
+    limit = limit +10;
+    
+    $("#more").remove();
+    getResults();
+    $("#moreButtonDiv").append("<button id='more' class='button first' onclick='moreGif();'>Load More Gifs</button>")
 }
 
 function appendCharButton () {
@@ -125,10 +199,11 @@ function setupButton (words) {
 }
 
 function gifAnime () {
-    $(".gifImage").on("click", function(event) {
-        let gifId = event.currentTarget.id;
-        console.log (gifId)
-        let i = gifId.substring(3,4);
+    $(".gifImgFrame").on("click", function(event) {
+        console.log ("anime click");
+        gifId = event.currentTarget.id;
+        let i = gifId.match(/\d/g);
+        i = i.join("");
         gifAnimeIdString = "#img" + i;
 
         if (isImgStill === true) {
